10b91a4ea4798e0f2db76c8d6a6fe394efb33518
Lef. Chrysos <boardproblems@gmail.com>
2024-12-10 20:01:14 +0200

    My First Commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Η κλάση HelloWorld εκτυπώνει το μήνυμα "Hello Coding Factory!" στην οθόνη.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*
+         *
+         */
+        System.out.println("Hello Coding Factory!");
+    }
+}
